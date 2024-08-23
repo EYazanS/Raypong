16b0920291fd625893e4e4385dd3e80f729349da
Yazan Salameh <yazan@serenbit.ps>
2024-08-23 22:52:11 +0300

    Fix window title name

--- src/main.c
+++ src/main.c
@@ -43,7 +43,7 @@ int main(void) {
 
     reset_game(constants, &game_state);
 
-    InitWindow(constants.ScreenWidth, constants.ScreenHeight, "Snakrray");
+    InitWindow(constants.ScreenWidth, constants.ScreenHeight, "Raypong");
 
     SetTargetFPS(60);
 
